5963b96ba230c54aab0214934ab8a5524dd2be92
johnnynode <johnnynode@gmail.com>
2017-10-26 14:01:44 +0800

    test regular-expressions

--- basic/regular-expressions.go
+++ basic/regular-expressions.go
@@ -14,7 +14,6 @@ func main() {
 	fmt.Println(r.MatchString("peach"))
 	fmt.Println(r.FindString("peach punch"))
 	fmt.Println(r.FindStringIndex("peach punch"))
-	fmt.Println(r.FindStringIndex("peach punch"))
 	fmt.Println(r.FindStringSubmatch("peach punch"))
 	fmt.Println(r.FindStringSubmatchIndex("peach punch"))
 	fmt.Println(r.FindAllString("peach punch pinch", -1))
@@ -32,3 +31,22 @@ func main() {
 	out := r.ReplaceAllFunc(in, bytes.ToUpper)
 	fmt.Println(string(out))
 }
+
+/*
+
+$ go run basic/regular-expressions.go
+true
+true
+peach
+[0 5]
+[peach ea]
+[0 5 1 3]
+[peach punch pinch]
+[[0 5 1 3] [6 11 7 9] [12 17 13 15]]
+[peach punch]
+true
+p([a-z]+)ch
+a <fruit>
+a PEACH
+
+*/
