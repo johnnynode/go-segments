9507ae2a26b23ad98a0aacfaa4902279f24b80e4
johnnynode <johnnynode@gmail.com>
2017-10-27 09:08:47 +0800

    test reading-files

--- basic/reading-files.go
+++ basic/reading-files.go
@@ -9,7 +9,7 @@ import (
 )
 
 func check(e error) {
-	if e != nil{
+	if e != nil {
 		panic(e)
 	}
 }
@@ -26,20 +26,20 @@ func main() {
 	check(err)
 	fmt.Printf("%d bytes: %s\n", n1, string(b1))
 
-	o2, err := f.Seek(6,0)
+	o2, err := f.Seek(6, 0)
 	check(err)
 	b2 := make([]byte, 2)
 	n2, err := f.Read(b2)
 	check(err)
 	fmt.Printf("%d bytes @ %d: %s\n", n2, o2, string(b2))
-	o3, err := f.Seek(6,0)
+	o3, err := f.Seek(6, 0)
 	check(err)
 	b3 := make([]byte, 2)
 	n3, err := io.ReadAtLeast(f, b3, 2)
 	check(err)
 	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
 
-	_, err = f.Seek(0,0)
+	_, err = f.Seek(0, 0)
 	check(err)
 
 	r4 := bufio.NewReader(f)
