8a1f7d5164831ee13759a448524a7e9eb7bbf31d
johnnynode <johnnynode@gmail.com>
2017-10-30 13:35:04 +0800

    recursion

---
+++ Basic/recursion.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(7)) // 5040 // 7 * 6 * 5 * 4 * 3 * 2 * 1 * 1
+}
+
+/*
+
+$ go run Basic/recursion.go
+5040
+
+*/
