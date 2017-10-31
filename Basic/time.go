111af585a62e39198f49ddd97d32c55e203d7a6e
johnnynode <johnnynode@gmail.com>
2017-10-31 20:18:54 +0800

    time

---
+++ Basic/time.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	p := fmt.Println
+
+	now := time.Now()
+	p(now)
+
+	then := time.Date(
+		2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+	p(then)
+
+	p(then.Year())
+	p(then.Month())
+	p(then.Day())
+	p(then.Hour())
+	p(then.Minute())
+	p(then.Second())
+	p(then.Nanosecond())
+	p(then.Location())
+	p(then.Weekday())
+	p(then.Before(now))
+	p(then.After(now))
+	p(then.Equal(now))
+
+	diff := now.Sub(then)
+	p(diff)
+
+	p(diff.Hours())
+	p(diff.Minutes())
+	p(diff.Seconds())
+	p(diff.Nanoseconds())
+
+	p(then.Add(diff))
+	p(then.Add(-diff))
+}
+
+/*
+
+$ go run Basic/time.go
+2017-10-31 20:18:28.0212094 +0800 CST
+2009-11-17 20:34:58.651387237 +0000 UTC
+2009
+November
+17
+20
+34
+58
+651387237
+UTC
+Tuesday
+true
+false
+false
+69711h43m29.369822163s
+69711.7248249506
+4.182703489497036e+06
+2.5096220936982217e+08
+250962209369822163
+2017-10-31 12:18:28.0212094 +0000 UTC
+2001-12-05 04:51:29.281565074 +0000 UTC
+
+*/
+
