555ffffac443de46614b8ff42b91537e02723f2c
johnnynode <johnnynode@gmail.com>
2017-10-31 21:46:37 +0800

    signals

---
+++ Basic/signals.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
+
+/*
+
+$ go run Basic/signals.go
+awaiting signal
+^C
+interrupt
+exiting
+
+*/
