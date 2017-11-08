d72e4e184cd0d08f7e15d58911734919ba5cce7d
johnnynode <johnnynode@gmail.com>
2017-11-08 08:47:46 +0800

    closures

---
+++ Basic/closures.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	nextInts := intSeq()
+	fmt.Println(nextInts())
+}
+
+/*
+$ go run Basic/closures.go
+1
+2
+3
+1
+
+*/
