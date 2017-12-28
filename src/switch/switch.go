800a657ce3813f0ab96b896c52a60fcc8ae76477
johnnynode <johnnynode@gmail.com>
2017-12-28 08:47:26 +0800

    switch

---
+++ src/switch/switch.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I am an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
+
+/*
+$ go run src/switch/switch.go
+Write 2 as two
+It's a weekday
+It's before noon
+I'm a bool
+I am an int
+Don't know type string
+*/
