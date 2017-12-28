54210c6af426136b6cbc21fd24b4459b845834e5
johnnynode <johnnynode@gmail.com>
2017-12-28 08:25:42 +0800

    variables

---
+++ src/variables/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "initial"
+	fmt.Println(a) // initial
+
+	var b,c int = 1, 2
+	fmt.Println(b, c) // 1 2
+
+	var d = true
+	fmt.Println(d) // true
+	
+	var e int
+	fmt.Println(e) // 0
+
+	f := "short"
+	fmt.Println(f) // short
+}
