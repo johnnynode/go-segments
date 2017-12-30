2caaa5266ecad281fa864cb9df9aa833e7791009
johnnynode <johnnynode@gmail.com>
2017-12-31 07:46:18 +0800

    structs

---
+++ src/structs/structs.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age int
+}
+
+func main() {
+	fmt.Println(person{"Bob", 20})
+	fmt.Println(person{name:"Alice", age:30})
+	fmt.Println(person{name:"Fred"})
+
+	fmt.Println(&person{name:"Ann", age:40})
+	s :=person{name:"Sean", age:50}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
+}
+
+/*
+$ go run src/structs/structs.go
+{Bob 20}
+{Alice 30}
+{Fred 0}
+&{Ann 40}
+Sean
+50
+51
+*/
