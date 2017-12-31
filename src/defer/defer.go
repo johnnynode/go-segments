0b12018bb92522b00df32afb240d4d55102d3e35
johnnynode <johnnynode@gmail.com>
2017-12-31 17:44:46 +0800

    defer

---
+++ src/defer/defer.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+import "os"
+
+func createFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File)  {
+	fmt.Println("writing")
+	fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	f.Close()
+}
+
+func main() {
+	f := createFile("./test/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
+}
+
+/*
+$ go run src/defer/defer.go
+creating
+writing
+closing
+*/
