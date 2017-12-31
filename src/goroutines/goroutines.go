31d001d540702c3b15fb4cc623582bc2d36b6fbf
johnnynode <johnnynode@gmail.com>
2017-12-31 08:22:28 +0800

    goroutines

---
+++ src/goroutines/goroutines.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func f(from string) {
+	for i := 0; i<3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("done")
+}
+
+/*
+$ go run src/goroutines/goroutines.go
+direct : 0
+direct : 1
+direct : 2
+goroutine : 0
+goroutine : 1
+goroutine : 2
+going
+<enter>
+done
+*/
